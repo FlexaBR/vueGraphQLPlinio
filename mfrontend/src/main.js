7984b41948c4728cd7b215d8d679fbe64cf8ba90
FlexaBR <r.gutheil@terra.com.br>
2019-07-06 11:25:23 -0300

    atualizando com correcao do nodemon no backend

--- mfrontend/src/main.js
+++ mfrontend/src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import '@/plugins/vuelidate'
+import '@/plugins/vuetify'
 
 Vue.config.productionTip = false
 
--- mfrontend/src/plugins/vuetify.js
+++ mfrontend/src/plugins/vuetify.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
 import pt from 'vuetify/es5/locale/pt'
+import colors from 'vuetify/es5/util/colors'
+import 'vuetify/src/stylus/app.styl'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
@@ -9,4 +12,12 @@ Vue.use(Vuetify, {
     locales: { pt },
     current: 'pt'
   },
+  theme: {
+    primary: colors.teal.darken1,
+    accent: colors.indigo.darken1,
+    info: colors.blue,
+    error: colors.pink.darken2,
+    success: colors.teal.lighten1,
+    warning: colors.purple.darken1
+  }
 })
