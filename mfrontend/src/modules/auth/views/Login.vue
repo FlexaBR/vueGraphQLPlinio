a47a70489c254c89e0f0ba51f10f9d914ef49a4d
FlexaBR <r.gutheil@terra.com.br>
2019-07-06 21:25:28 -0300

    ate aula 410

--- mfrontend/src/modules/auth/views/Login.vue
+++ mfrontend/src/modules/auth/views/Login.vue
@@ -18,16 +18,35 @@
             color="primary"
             dark
           >
-            <v-toolbar-title>Login</v-toolbar-title>
+            <v-toolbar-title>{{ texts.toolbar }}</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-progress-circular
+              v-show="isLoading"
+              indeterminate
+              color="white"
+              width="2"
+            ></v-progress-circular>
           </v-toolbar>
 
           <v-card-text>
             <v-form>
               <v-text-field
+                v-if="!isLogin"
+                prepend-icon="person"
+                name="name"
+                label="Nome"
+                type="text"
+                :error-messages="nameErrors"
+                :success="!$v.user.name.$invalid"
+                v-model.trim="$v.user.name.$model"
+              ></v-text-field>
+              <v-text-field
                 prepend-icon="email"
                 name="email"
                 label="email"
                 type="email"
+                :error-messages="emailErrors"
+                :success="!$v.user.email.$invalid"
                 v-model.trim="$v.user.email.$model"
               ></v-text-field>
               <v-text-field
@@ -35,6 +54,8 @@
                 name="password"
                 label="Senha"
                 type="password"
+                :error-messages="passwordErrors"
+                :success="!$v.user.password.$invalid"
                 v-model.trim="$v.user.password.$model"
               ></v-text-field>
             </v-form>
@@ -42,9 +63,9 @@
               block
               depressed
               color="secondary"
-              @click="log"
+              @click="isLogin = !isLogin"
             >
-              Criar Conta
+              {{ texts.button }}
             </v-btn>
           </v-card-text>
 
@@ -55,8 +76,23 @@
               color="primary"
               large
               @click="submit"
-            >Login</v-btn>
+            >{{ texts.toolbar }}</v-btn>
           </v-card-actions>
+
+          <v-snackbar
+            v-model="showSnackbar"
+            top
+          >
+            {{ error }}
+            <v-btn
+              color="pink"
+              flat
+              icon
+              @click="showSnackbar = false"
+            >
+              <v-icon>close</v-icon>
+            </v-btn>
+          </v-snackbar>
 
         </v-card>
       </v-flex>
@@ -67,33 +103,92 @@
 
 <script>
 import { required, email, minLength } from 'vuelidate/lib/validators'
+import AuthService from './../services/auth-service'
+import { formatError } from '@/utils'
 
 export default {
   name: 'Login',
   data: () => ({
+    error: undefined,
+    isLogin: true,
+    isLoading: false,
+    showSnackbar: false,
     user: {
+      name: '',
       email: '',
       password: ''
     }
   }),
-  validations: {
-    user: {
-      email: {
-        required,
-        email
-      },
-      password: {
-        required,
-        minLength: minLength(6)
+  validations () {
+    const validations = {
+      user: {
+        email: {
+          required,
+          email
+        },
+        password: {
+          required,
+          minLength: minLength(6)
+        }
+      }
+    }
+    if (this.isLogin) { return validations }
+    return {
+      user: {
+        ...validations.user,
+        name: {
+          required,
+          minLength: minLength(3)
+        }
       }
     }
   },
-  methods: {
-    log () {
-      console.log('Vuelidate: ', this.$v)
+  computed: {
+    texts () {
+      return this.isLogin
+        ? { toolbar: 'Entrar', button: 'Criar conta' }
+        : { toolbar: 'Criar conta', button: 'Já tenho uma conta' }
     },
-    submit () {
-      console.log('user: ', this.user)
+    nameErrors () {
+      const errors = []
+      const name = this.$v.user.name
+      if (!name.$dirty) { return errors }
+      !name.required && errors.push('Nome é obrigatório!')
+      !name.minLength && errors.push(`Insira pelo menos ${name.$params.minLength.min} caracteres!`)
+      return errors
+    },
+    emailErrors () {
+      const errors = []
+      const email = this.$v.user.email
+      if (!email.$dirty) { return errors }
+      !email.required && errors.push('Email é obrigatório!')
+      !email.email && errors.push('Insira um email válido!')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      const password = this.$v.user.password
+      if (!password.$dirty) { return errors }
+      !password.required && errors.push('Senha é obrigatória!')
+      !password.minLength && errors.push(`Insira pelo menos ${password.$params.minLength.min} caracteres!`)
+      return errors
+    }
+  },
+  methods: {
+    async submit () {
+      this.isLoading = true
+      try {
+        this.isLogin  
+          ? await AuthService.login(this.user)
+          : await AuthService.signup(this.user)
+        this.$router.push(this.$route.query.redirect || '/dashboard')
+      } catch (error) {
+        console.log(error)
+        this.error = formatError(error.message)
+        this.showSnackbar = true
+      } finally {
+        this.isLoading = false
+      }
     }
   }
 }
